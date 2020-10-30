ac075a7da6fb2ca794817fc3dc755aeba5181270
Jhonny <jhonnyguilcapi@gmail.com>
2020-10-30 00:33:27 -0500

    Agregar archivos de proyecto.

---
+++ TareaSemana3JGuilcapi/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TareaSemana3JGuilcapi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+
+            //Condicion
+            if (user == "estudiante2020" && pass == "uisrael2020")
+            {
+                await Navigation.PushAsync( new Pagina2(user, pass));
+            }
+            else
+            {
+                await DisplayAlert("Alerta", "Usuario incorrecto", "OK");
+            }
+        }
+    }
+}
---
+++ TareaSemana3JGuilcapi/Pagina2.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TareaSemana3JGuilcapi
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina2 : ContentPage
+    {
+        public Pagina2(string user, string pass)
+        {
+            InitializeComponent();
+            string usuario = IblUser.Text;
+            IblUser.Text = usuario + user;
+        }
+    }
+}
